40ec6d736b213b25ffbe48b6616fe4908123581d
Rafael <rafaelproinfo@gmail.com>
2022-07-12 02:15:48 -0300

    Update Heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { TarefaModule } from './tarefa/tarefa.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+   /* TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
@@ -16,6 +16,17 @@ import { TarefaModule } from './tarefa/tarefa.module';
       database: 'db_todo',
       entities: [Tarefa, Categoria],
       synchronize: true,
+    }),*/
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      logging: false,
+      dropSchema: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      synchronize: true,
+      autoLoadEntities: true,
     }),
     CategoriaModule,
     TarefaModule,
